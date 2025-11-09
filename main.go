5669744ecc956165debebcea8e503cd53ce5d019
victro-rios <victrohugo100@gmail.com>
2025-11-09 13:24:05 -0800

    Adding first endpoint

--- main.go
+++ main.go
@@ -9,11 +9,16 @@ func main() {
 	fmt.Println("Initializing server...")
 	port := ":8080"
 	mux := http.NewServeMux()
-	fileServerHandler := http.FileServer(http.Dir("."))
+	mux.HandleFunc("/healthz", healthHandler)
 	server := &http.Server{
-		Addr: port,
+		Addr:    port,
 		Handler: mux,
 	}
-	mux.Handle("/", fileServerHandler)
 	server.ListenAndServe()
+}
+
+func healthHandler(responseWriter http.ResponseWriter, request *http.Request) {
+	responseWriter.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	responseWriter.WriteHeader(200)
+	responseWriter.Write([]byte("OK"))
 }
