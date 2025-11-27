ed63ef9214640b4c47c0fa531a1c90b45a229132
victro-rios <victrohugo100@gmail.com>
2025-11-26 18:44:30 -0800

    Adding atomic counter to persist value

--- main.go
+++ main.go
@@ -3,25 +3,27 @@ package ratelimiter
 import (
 	"errors"
 	"fmt"
+	"sync/atomic"
 )
 
 type RateLimiter struct {
 	cfg     Config
-	buckets map[string]int
+	buckets map[string]*atomic.Int32
 }
 
 func New(config Config) *RateLimiter {
-	return &RateLimiter{cfg: config, buckets: make(map[string]int)}
+	return &RateLimiter{cfg: config, buckets: make(map[string]*atomic.Int32)}
 }
 
 func (rateLimiter *RateLimiter) Consume(key string, tokensToConsume uint8) error {
 	_, exists := rateLimiter.buckets[key]
 	if !exists {
-		rateLimiter.buckets[key] = rateLimiter.cfg.MaximumBurst
+		rateLimiter.buckets[key] = &atomic.Int32{}
+		rateLimiter.buckets[key].Store(10)
 	}
-	rateLimiter.buckets[key] -= 1
-	fmt.Printf("Consuming... %d tokens left", rateLimiter.buckets[key])
-	if rateLimiter.buckets[key] <= 0 {
+	rateLimiter.buckets[key].Add(1)
+	fmt.Printf("Consuming... %d tokens left", rateLimiter.buckets[key].Load())
+	if rateLimiter.buckets[key].Load() <= 0 {
 		fmt.Printf("Throwing error 429")
 		return errors.New("Too many requests")
 	}
